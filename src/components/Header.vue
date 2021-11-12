301e0414a2fe196f47961049b5fa0702ccf7c79b
feix <feix@didiglobal.com>
2021-11-13 00:01:26 +0800

    fix:t

---
+++ src/components/Header.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="nav">
+    <div class="logo">
+      <img src="../assets/logo.png" alt="" class="logo-img" />
+    </div>
+    <div class="list">
+      <div class="item bigger">
+        <a href="/" class="link">Treasury</a>
+      </div>
+      <div class="item">
+        <a href="/" class="link">Trade</a>
+      </div>
+      <div class="item">
+        <a href="/" class="link">Win</a>
+      </div>
+      <div class="item">
+        <a href="/" class="link">Earn</a>
+      </div>
+      <div class="item nft">
+        <div class="p">
+          <a href="/" class="link">NFT</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Nav",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style lang="less">
+.logo {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background: #000000;
+  vertical-align: center;
+  position: relative;
+  .logo-img {
+    width: 17px;
+    height: 17px;
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+
+.nav {
+  padding-top: 14px;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  transition: top 0.2s ease 0s;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  background-color: #46369c;
+  border-bottom: 1px solid #46369c;
+  z-index: 20;
+  transform: translate3d(0px, 0px, 0px);
+  padding-left: 16px;
+  padding-right: 16px;
+  .list {
+    margin-left: 24px;
+    flex-grow: 1;
+    display: flex;
+    .item.bigger {
+      .link {
+        margin-top: 10px;
+        width: 116px;
+        height: 22px;
+        font-size: 24px;
+        font-family: Arial Black;
+        font-weight: 400;
+        text-decoration: none;
+        color: #ffffff;
+      }
+    }
+
+    .item {
+      padding-top: 4px;
+      padding-bottom: 4px;
+      .p {
+        width: 70px;
+
+        border: 2px solid #33f8ff;
+        border-radius: 17px;
+        font-size: 22px;
+        font-family: DIN Alternate;
+        font-weight: bold;
+        color: #33f8ff;
+        width: 60px;
+        height: 29px;
+        line-height: 29px;
+        position: relative;
+        top: 4px;
+        .link {
+          color: #33f8ff;
+        }
+      }
+      &.nft {
+        .link {
+          height: 34px;
+          width: 70px;
+        }
+      }
+      .link {
+        position: relative;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        color: rgb(122, 110, 170);
+        font-size: 16px;
+        font-weight: 400;
+        padding: 0px 16px;
+        text-decoration: none;
+        height: 48px;
+      }
+    }
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,66 +1,68 @@
 <template>
   <div class="container">
-        <section class="nav"></section>
-        <section class="header">
-            <div class="left">
-                <p class="fs72 bold">NFT Market</p>
-                <p class="fs30 mt40">Buy and Sell NFTs on Binance Smart Chain</p>
-                <div class="mt60">
-                    <button class="base-btn overview mr40">Overview</button>
-                    <button class="base-btn bold fs20">Collections</button>
-                </div>
+    <NavHeader />
+    <section class="header">
+      <div class="left">
+        <p class="fs72 bold">NFT Market</p>
+        <p class="fs30 mt40">Buy and Sell NFTs on Binance Smart Chain</p>
+        <div class="mt60">
+          <button class="base-btn overview mr40">Overview</button>
+          <button class="base-btn bold fs20">Collections</button>
+        </div>
+      </div>
+      <div class="right">
+        <input class="base-btn search" placeholder="search address" />
+      </div>
+    </section>
+    <section class="content">
+      <div class="sub-header">
+        <h3 class="fs30">Hot Collections</h3>
+        <div class="base-btn viewall">View All</div>
+      </div>
+      <div class="product">
+        <div class="mb40 item" v-for="item in 6" :key="item">
+          <img src="" alt="" />
+          <div class="info">
+            <div class="left"></div>
+            <div class="right hot fs24">
+              <p>Pancake Squad</p>
+              <p>968.0684</p>
             </div>
-            <div class="right">
-                <input class="base-btn search" placeholder="search address" />
+          </div>
+        </div>
+      </div>
+      <div class="sub-header">
+        <h3 class="fs30">Newest Arrivals</h3>
+        <div class="base-btn viewall">View All</div>
+      </div>
+      <div class="product">
+        <div class="mb40 item item2" v-for="item in 3" :key="item">
+          <img src="" alt="" />
+          <div class="info2">
+            <div class="newest fs24">
+              <p class="fs20">Pancake Bunnies</p>
+              <p class="fs18">Flipsie Easter '21</p>
             </div>
-        </section>
-        <section class="content">
-            <div class="sub-header">
-                <h3 class="fs30">Hot Collections</h3>
-                <div class="base-btn viewall">View All</div>
+            <div class="bottom">
+              <div>Lowest price</div>
+              <div class="price">
+                <span>$18.98</span>
+                <span>0.0785</span>
+              </div>
             </div>
-            <div class="product">
-                <div class="mb40 item" v-for="item in 6" :key="item">
-                    <img src="" alt="">
-                    <div class="info">
-                        <div class="left"></div>
-                        <div class="right hot fs24">
-                            <p>Pancake Squad</p>
-                            <p>968.0684</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="sub-header">
-                <h3 class="fs30">Newest Arrivals </h3>
-                <div class="base-btn viewall">View All</div>
-            </div>
-            <div class="product">
-                <div class="mb40 item item2" v-for="item in 3" :key="item">
-                    <img src="" alt="">
-                    <div class="info2">
-                        <div class="newest fs24">
-                            <p class="fs20">Pancake Bunnies  </p>
-                            <p class="fs18">Flipsie Easter '21</p>
-                        </div>
-                        <div class="bottom">
-                            <div>Lowest price</div>
-                            <div class="price">
-                                <span>$18.98</span>
-                                <span>0.0785</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="footer"></section>
-    </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="footer"></section>
+  </div>
 </template>
 
 <script>
+import NavHeader from "./Header.vue";
 export default {
   name: "HelloWorld",
+  components: { NavHeader },
   props: {
     msg: String,
   },
@@ -69,133 +71,157 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
-body, p, div {
-    margin: 0;
-    padding: 0;
+body,
+p,
+div {
+  margin: 0;
+  padding: 0;
 }
 #app {
-    background: url('../assets/bj1.png') no-repeat;
-    background-size: cover;
-    
+  background: url("../assets/bj1.png") no-repeat;
+  background-size: cover;
 }
 .container {
-    margin: 0 auto;
+  margin: 0 auto;
+  position: relative;
+  width: 100%;
 }
 .header {
-    color: #fff;
-    position: relative;
+  color: #fff;
+  position: relative;
 }
 .header .right input {
-    position: absolute;
-    right: 0;
-    top: 0;
-    margin-top: 25px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: 25px;
 }
-.header .right input::-webkit-input-placeholder { 
-    color: #fff; 
-    font-size: 20px;
+.header .right input::-webkit-input-placeholder {
+  color: #fff;
+  font-size: 20px;
 }
-.fs72 { font-size: 72px;}
-.fs30 { font-size: 30px;}
-.fs20 { font-size: 20px;}
-.fs18 { font-size: 18px;}
-.fs24 { font-size: 24px;}
-.bold { font-weight: bold;}
-.mt60 { margin-top: 60px;}
-.mt40 { margin-top: 40px;}
-.mb40 { margin-bottom: 40px !important;}
-.mb60 { margin-bottom: 60px;}
-.ml40 { margin-left: 40px;}
-.mr40 { margin-right: 40px;}
+.fs72 {
+  font-size: 72px;
+}
+.fs30 {
+  font-size: 30px;
+}
+.fs20 {
+  font-size: 20px;
+}
+.fs18 {
+  font-size: 18px;
+}
+.fs24 {
+  font-size: 24px;
+}
+.bold {
+  font-weight: bold;
+}
+.mt60 {
+  margin-top: 60px;
+}
+.mt40 {
+  margin-top: 40px;
+}
+.mb40 {
+  margin-bottom: 40px !important;
+}
+.mb60 {
+  margin-bottom: 60px;
+}
+.ml40 {
+  margin-left: 40px;
+}
+.mr40 {
+  margin-right: 40px;
+}
 
 .base-btn {
-    border-radius: 25px;
-    border: 1px solid #33F8FF;
-    padding: 15px 35px;
-    background: inherit;
-    color: #fff;
+  border-radius: 25px;
+  border: 1px solid #33f8ff;
+  padding: 15px 35px;
+  background: inherit;
+  color: #fff;
 }
 .overview {
-    color: #212222;
-    font-weight: bold;
-    font-size: 20px;
-    background: #33F8FF;
+  color: #212222;
+  font-weight: bold;
+  font-size: 20px;
+  background: #33f8ff;
 }
 .content {
-    color: #fff;
+  color: #fff;
 }
 .sub-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .sub-header .viewall {
-    color: #33F8FF;
+  color: #33f8ff;
 }
 .content .product {
-    display: flex;
-    flex-wrap: wrap;
-
+  display: flex;
+  flex-wrap: wrap;
 }
 .content .product .item {
-    width: 380px;
-    height: 400px;
-    margin: 0 auto;
-    background: linear-gradient(60deg, #9670F6, #51AEFE);
-    border-radius: 20px;
-    padding: 10px;
-    box-sizing: border-box;
-    
+  width: 380px;
+  height: 400px;
+  margin: 0 auto;
+  background: linear-gradient(60deg, #9670f6, #51aefe);
+  border-radius: 20px;
+  padding: 10px;
+  box-sizing: border-box;
 }
 .content .product .item2 {
-    height: 520px;
-    position: relative;
+  height: 520px;
+  position: relative;
 }
 .content .item img {
-    width: 360px;
-    height: 270px;
-    background: #ccc;
+  width: 360px;
+  height: 270px;
+  background: #ccc;
 }
 .content .item .info {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 15px;
 }
 .content .item .info .left {
-    width: 90px;
-    height: 90px;
-    border-radius: 50%;
-    background: #ccc;
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  background: #ccc;
 }
 .content .item .info .hot {
-    text-align: right;
+  text-align: right;
 }
 .content .item2 img {
-    width: 360px;
-    height: 330px;
-    background: #ccc;
+  width: 360px;
+  height: 330px;
+  background: #ccc;
 }
 .content .item .info2 .bottom {
-    position: absolute;
-    bottom: 0;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    border-top: 1px solid #2045C0;
-    width: 95%;
-    justify-content: space-between;
+  position: absolute;
+  bottom: 0;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  border-top: 1px solid #2045c0;
+  width: 95%;
+  justify-content: space-between;
 }
-
 
 @media screen and (max-width: 1440px) {
-    .container {
-        max-width: 1200px;
-    }
+  .container {
+    max-width: 1200px;
+  }
 }
 @media screen and (min-width: 1920px) {
-    .container {
-        max-width: 1600px;
-    }
+  .container {
+    max-width: 1600px;
+  }
 }
 </style>
