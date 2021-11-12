1ef36bf9f4af7509c992d0de81fe12273082f3d2
feix <feix@didiglobal.com>
2021-11-12 22:48:19 +0800

    fix:r

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from "vue-router";
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Home from "./components/HelloWorld.vue";
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes: [{ path: "/", component: Home }], // short for `routes: routes`
+});
+let app = createApp(App)
+app.use(router)
+app.mount('#app')
