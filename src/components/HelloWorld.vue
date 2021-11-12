e89396b3f52d840deb52023a01c7f6296e5ab38d
feix <feix@didiglobal.com>
2021-11-12 22:02:30 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <section class="nav"></section>
+    <section class="header">
+      <div class="left">
+        <p class="fs72 bold">NFT Market</p>
+        <p class="fs30 mt40">Buy and Sell NFTs on Binance Smart Chain</p>
+        <div class="mt60">
+          <button class="base-btn overview mr40">Overview</button>
+          <button class="base-btn bold fs20">Collections</button>
+        </div>
+      </div>
+      <div class="right">
+        <input class="base-btn search" placeholder="search address" />
+      </div>
+    </section>
+    <section class="content">
+      <div class="sub-header">
+        <h3>Hot Collections</h3>
+        <div class="base-btn">View All</div>
+      </div>
+      <div class="main"></div>
+    </section>
+    <section class="footer"></section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<style lang="less">
+body,
+p,
+div {
+  margin: 0;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#app {
+  background: url("../assets/bj1.png") no-repeat;
+  background-size: cover;
 }
-a {
-  color: #42b983;
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.header {
+  color: #fff;
+  position: relative;
+}
+.header .right input {
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: 25px;
+}
+.header .right input::-webkit-input-placeholder {
+  color: #fff;
+  font-size: 20px;
+}
+.fs72 {
+  font-size: 72px;
+}
+.fs30 {
+  font-size: 30px;
+}
+.fs20 {
+  font-size: 20px;
+}
+.bold {
+  font-weight: bold;
+}
+.mt60 {
+  margin-top: 60px;
+}
+.mt40 {
+  margin-top: 40px;
+}
+.mb60 {
+  margin-bottom: 60px;
+}
+.ml40 {
+  margin-left: 40px;
+}
+.mr40 {
+  margin-right: 40px;
+}
+
+.base-btn {
+  border-radius: 25px;
+  border: 1px solid #33f8ff;
+  padding: 15px 35px;
+  background: inherit;
+  color: #fff;
+}
+.overview {
+  color: #212222;
+  font-weight: bold;
+  font-size: 20px;
+  background: #33f8ff;
+}
+.content {
+  color: #fff;
+}
+.sub-header {
+  position: relative;
+}
+.sub-header .viewall {
 }
 </style>
